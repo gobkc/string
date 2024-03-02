66ee8611499fb34668c7179d3d0c1b7a05710ae3
xiongjianbo <10044375@qq.com>
2024-03-02 21:54:24 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub trait StringExtend {
-    fn substr(&self, start_pos: i32,end_pos:i32) -> String;
+    fn substr(&self, start_pos: usize, end_pos:usize) -> String;
 }
 
 impl StringExtend for str {
-    fn substr(&self, start_pos: i32,end_pos:i32) -> String {
+    fn substr(&self, start_pos: usize, end_pos:usize) -> String {
         let slice = &self[start_pos..end_pos];
         slice.to_string()
     }
